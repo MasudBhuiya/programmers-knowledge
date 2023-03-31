531a290b3258165ecee2e5c088bc6845c59bbf7d
MasudBhuiya <masudbhuiyan1415@gmail.com>
2023-03-31 16:53:23 +0600

    make moblie responsive

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -3,32 +3,32 @@ import Details from '../Details/Details';
 import './Cart.css'
 import Blog from '../Blog/Blog';
 
-const Cart = ({handleTime, time, handleTitle, title, bookmark}) => {
+const Cart = ({ handleTime, time, handleTitle, title, bookmark }) => {
     // console.log(title)
     const [cart, setCart] = useState([])
     const [totalTime, setTotalTime] = useState(time)
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('products.json')
-        .then(res => res.json())
-        .then(data => setCart(data))
-    },[])
-    useEffect(()=>{
+            .then(res => res.json())
+            .then(data => setCart(data))
+    }, [])
+    useEffect(() => {
         const totalTimes = localStorage.getItem('totalTime')
         setTotalTime(totalTimes);
-    },[time])
+    }, [time])
 
     return (
         <div className='cart-container'>
-            <div>
-            <Details cart ={cart} handleTime={handleTime} key={cart.id} handleTitle={handleTitle} title={title}></Details>
+            <div className='col'>
+                <Details cart={cart} handleTime={handleTime} key={cart.id} handleTitle={handleTitle} title={title}></Details>
             </div>
-            <div  className='sidebar'>
-            <h5 className='spend-time'>Spent time on read: <span>{totalTime}</span></h5>
-            <div >
-            <h4 style={{color:'black'}}>BookMarked Blogs: {bookmark} </h4>
-            <p style={{backgroundColor:'aliceblue', color:'black', paddingBottom:'10px'}}><span >{title}</span></p>
-            </div>
+            <div className='sidebar'>
+                <h5 className='spend-time'>Spent time on read: <span>{totalTime}</span></h5>
+                <div >
+                    <h4 style={{ color: 'black' }}>BookMarked Blogs: {bookmark} </h4>
+                    <p style={{ backgroundColor: 'aliceblue', color: 'black', paddingBottom: '10px' }}><span >{title}</span></p>
+                </div>
             </div>
             <Blog></Blog>
         </div>
